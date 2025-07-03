554aed744ad2d72f8434e78b2284e99f72ab990c
Raphael Salviano <raphael_salviano@outlook.com>
2025-07-03 16:48:14 -0300

    refactor: simplify type export and remove unnecessary namespace

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
+export * from './types';
+export * from './aggregation';
+export * from './operations';
+export * from './constants';
 import aggregate from './aggregation';
-import * as Types from './types';
 
 const ReactAggregation = aggregate;
 
-export namespace ReactAggregation {
-  export type AggregationPipeline = Types.AggregationPipeline;
-  export type PipelineStage = Types.PipelineStage;
-  export type DefaultObject = Types.DefaultObject;
-  export type DatabaseAdapter = Types.DatabaseAdapter;
-  export type DatabaseRules = Types.DatabaseRules;
-  export type DatabaseConfig = Types.DatabaseConfig;
-}
-
-export * from './types';
+export { ReactAggregation };
 export default ReactAggregation;
